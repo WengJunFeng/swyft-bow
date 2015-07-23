106190e1554d9d67c8f3d1538cc9848f87c1a213
acceleration3 <acceleration23@gmail.com>
2015-07-23 03:15:04 +0100

    Added support for Font name and size and name color for both PMs and Chatrooms.

--- SwyftBow/StringUtils.cpp
+++ SwyftBow/StringUtils.cpp
@@ -15,6 +15,14 @@ std::string entities[NUM_ENTITIES][2] = {
 
 double hue = 0;
 
+//TODO: Add GUI overlay to change these settings
+int loopLength = 30;
+float saturation = 0.8f;
+float value = 0.8f;
+string fontName = "Comic Sans MS";
+string nameColor = "CCC";
+string fontSize = "11";
+
 string ReplaceString(std::string subject, const std::string& search, const std::string& replace)
 {
 	size_t pos = 0;
@@ -28,9 +36,6 @@ string ReplaceString(std::string subject, const std::string& search, const std::
 	return subject;
 }
 
-//Name color regex: <n(\w*)\/>
-//Message filter regex: b?msg:(.*):(.*)
-
 bool StringUtils::ProcessMessage(string* text)
 {
 	regex filter("(b?msg:([^:]*):)(.*)\r\n");
@@ -41,19 +46,30 @@ bool StringUtils::ProcessMessage(string* text)
 		std::string prefix;
 		std::string param;
 		std::string message;
-
+		
 		regex_match(*text, sm, filter);
 
 		prefix = sm[1];
 		param = sm[2];
 		message = sm[3];
 
-		message = StringUtils::StripHTML(message);
-		message = StringUtils::Rainbowify(message, (prefix[0] == 'b'));
+		bool isRoom = prefix[0] == 'b';
 
-		prefix.append("<n7/><m v=\"1\">");
-		message = prefix.append(message);
-		message.append("</m>\r\n");
+		message = StringUtils::StripHTML(message);
+		message = StringUtils::Rainbowify(message, isRoom);
+
+		if (isRoom)
+		{
+			prefix.append("<n" + nameColor + "/><f x" + fontSize + "=\"" + fontName + "\">");
+			message = prefix.append(message);
+			message.append("\r\n");
+		}
+		else
+		{
+			prefix.append("<n" + nameColor + "/><m v=\"1\">");
+			message = prefix.append(message);
+			message.append("</m>\r\n");
+		}
 
 		*text = message;
 		return true;
@@ -63,15 +79,8 @@ bool StringUtils::ProcessMessage(string* text)
 
 string StringUtils::Rainbowify(string text, bool isRoom)
 {
-	char strcolor[30];
+	char strcolor[200];
 	std::string outmsg;
-
-	//TODO: Add GUI overlay to change these settings
-	int loopLength = 30;
-	int fontSize = 11;
-	float saturation = 0.8f;
-	float value = 0.8f;
-	
 
 	for (unsigned int i = 0; i < text.size(); i+=2)
 	{
@@ -84,15 +93,16 @@ string StringUtils::Rainbowify(string text, bool isRoom)
 		
 		if (isRoom)
 		{
-			sprintf_s(strcolor, "<f x%d%02x%02x%02x=\"0\">", fontSize, (int)(color.r * 0xFF), (int)(color.g * 0xFF), (int)(color.b * 0xFF));
+			sprintf_s(strcolor, "<f x%02x%02x%02x=\"\">", (int)(color.r * 0xFF), (int)(color.g * 0xFF), (int)(color.b * 0xFF));
 			outmsg.append(strcolor);
-			outmsg.append(text.substr(i, 2));
+			outmsg.append(StringUtils::MakeHTML(text.substr(i, 2)));
+			outmsg.append("</f>");
 		}
 		else
 		{
-			sprintf_s(strcolor, "<g x%ds%02x%02x%02x=\"\">", fontSize, (int)(color.r * 0xFF), (int)(color.g * 0xFF), (int)(color.b * 0xFF));
+			sprintf_s(strcolor, "<g x%ss%02x%02x%02x=\"%s\">", fontSize.c_str(), (int)(color.r * 0xFF), (int)(color.g * 0xFF), (int)(color.b * 0xFF), fontName.c_str());
 			outmsg.append(strcolor);
-			outmsg.append(text.substr(i, 2));
+			outmsg.append(StringUtils::MakeHTML(text.substr(i, 2)));
 			outmsg.append("</g>");
 		}
 	}
@@ -102,10 +112,27 @@ string StringUtils::Rainbowify(string text, bool isRoom)
 
 string StringUtils::MakeHTML(string text)
 {
-	for (int i = 0; i < NUM_ENTITIES; i++)
-		text = ReplaceString(text, entities[i][1], entities[i][0]);
+	bool replaced = false;
+	string result;
 
-	return text;
+	for (unsigned int i = 0; i < text.size(); i++)
+	{
+		for (int j = 0; j < NUM_ENTITIES; j++)
+		{
+			if (text.substr(i, 1).compare(entities[j][1]) == 0)
+			{
+				result.append(entities[j][0]);
+				replaced = true;
+			}
+		}
+
+		if (replaced == false)
+				result.append(text.substr(i, 1));
+	
+		replaced = false;
+	}
+		
+	return result;
 }
 
 string StringUtils::StripHTML(string text)
